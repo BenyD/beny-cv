35f4e2de9a96f000dbdaf715a447f71ae66e7ea4
Beny Dishon <benydishon@gmail.com>
2024-09-01 20:08:08 +0530

    Dock Complete

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -93,9 +93,13 @@ const DATA = {
 
 export function Navbar() {
   return (
-    <div className="fixed bottom-0 mb-4 w-full">
+    <div className="pointer-events-none fixed bottom-0 mb-4 w-full origin-bottom">
+      <div className="fixed inset-x-0 bottom-0 h-16 w-full bg-background to-transparent backdrop-blur-lg [-webkit-mask-image:linear-gradient(to_top,black,transparent)] dark:bg-background"></div>
       <TooltipProvider>
-        <Dock direction="middle">
+        <Dock
+          direction="middle"
+          className="pointer-events-auto transform-gpu bg-background [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)] dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset] "
+        >
           {DATA.navbar.map((item) => (
             <DockIcon key={item.label}>
               <Tooltip>
